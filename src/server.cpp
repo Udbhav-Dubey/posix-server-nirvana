c299cd735f1ffccb62b1a10b38df37e6fa8f21ae
Udbhav-Dubey <udbhavdubey44@gmail.com>
2026-01-19 13:49:22 +0530

    poll started

--- src/server.cpp
+++ src/server.cpp
@@ -58,59 +58,6 @@ bool TcpServer::start(){
     return true;
 }
 void TcpServer::run(){
-    while(true){
-        sockaddr_in client_addr{};
-        socklen_t client_len=sizeof(client_addr);
-        int client_sock=accept(server_socket,(sockaddr *)&client_addr,&client_len);
-        if (client_sock<=-1){continue;}
-        std::thread(&TcpServer::handleClient,this,client_sock).detach();
-    }
-    }
-void TcpServer::handleClient(int client_sock){
-    auto StartTime=std::chrono::steady_clock::now();
-    char buffer[4096];
-    ssize_t n=recv(client_sock,buffer,sizeof(buffer),0);
-    if (n<0){
-        perror("recv");
-        std::cout << "connection removed \n";
-        close(client_sock);
-        return ;
-    }
-    buffer[n]='\0';
-    try{
-        std::string request(buffer);
-        std::string path="/";
-        size_t pos=request.find(" ");
-        if (pos!=std::string::npos){
-            size_t pos2=request.find(" ",pos+1);
-            if (pos2!=std::string::npos){
-                path=request.substr(pos+1,pos2-pos-1);
-            }
-        }
-        {
-            std::lock_guard<std::mutex>lock(cout_mutex);
-            std::cout << "[Thread " << std::this_thread::get_id()<<"] Request : " <<path;
-        }
-        std::cout << "    Request   " << path  ;
-        HttpResponse response=router.route(path);
-        std::string response_data=response.build();
-        size_t total=0;
-        while(total<response_data.size()){
-            ssize_t sent=send(client_sock,response_data.data()+total,response_data.size()-total,0);
-            if (sent<=0)break;
-            total+=sent;
-        }
-    }
-    catch(const std::exception&e){
-        std::cerr<<"[error] exceptions in handle clinet : " << e.what() << "\n";
-    }
-    close(client_sock);
-    auto end_time=std::chrono::steady_clock::now();
-    auto duration=end_time-StartTime;
-    auto ms=std::chrono::duration<double,std::milli>(duration).count();
-    {
-        std::lock_guard<std::mutex>lock(cout_mutex);
-        std::cout<<":: "<<std::fixed << std::setprecision(2)<<ms<<" ms\n";
-    }
+
 }
 
